b807b61a0de42cf0daca13c80d75745e79928edc
Heunsig <heun3344@gmail.com>
2024-09-21 22:45:53 +0900

    feat: style custom checkbox

--- src/components/CustomCheckbox.vue
+++ src/components/CustomCheckbox.vue
@@ -47,19 +47,20 @@ onMounted(() => {
 }
 .custom-checkbox .cbx span:first-child {
   position: relative;
-  width: 24px;
-  height: 24px;
-  border-radius: 50%;
+  width: 1rem;
+  height: 1rem;
   transform: scale(1);
   vertical-align: middle;
-  border: 1px solid #b9b8c3;
+  border: 1px solid #2e2e2e;
   transition: all 0.2s ease;
+  border-radius: 0.375rem;
+  background-color: #404348;
 }
 .custom-checkbox .cbx span:first-child svg {
   position: absolute;
   z-index: 1;
-  top: 8px;
-  left: 6px;
+  top: 2px;
+  left: 1px;
   fill: none;
   stroke: white;
   stroke-width: 2;
@@ -70,44 +71,15 @@ onMounted(() => {
   transition: all 0.3s ease;
   transition-delay: 0.1s;
   transform: translate3d(0, 0, 0);
-}
-.custom-checkbox .cbx span:first-child:before {
-  content: '';
-  width: 100%;
-  height: 100%;
-  background: #506eec;
-  display: block;
-  transform: scale(0);
-  opacity: 1;
-  border-radius: 50%;
-  transition-delay: 0.2s;
+  transform: scale(0.8);
 }
 
 .custom-checkbox .inp-cbx:checked + .cbx span:first-child {
-  border-color: #3c53c7;
-  background: #3c53c7;
-  animation: check-15 0.6s ease;
-}
-.custom-checkbox .inp-cbx:checked + .cbx span:first-child svg {
-  stroke-dashoffset: 0;
-}
-.custom-checkbox .inp-cbx:checked + .cbx span:first-child:before {
-  transform: scale(2.2);
-  opacity: 0;
-  transition: all 0.6s ease;
-}
-.custom-checkbox .inp-cbx:checked + .cbx span:last-child {
-  color: #b9b8c3;
-  transition: all 0.3s ease;
-}
-.custom-checkbox .inp-cbx:checked + .cbx span:last-child:after {
-  transform: scaleX(1);
-  transition: all 0.3s ease;
+  border-color: #000000;
+  background: #1e1e1e;
 }
 
-@keyframes check-15 {
-  50% {
-    transform: scale(1.2);
-  }
+.custom-checkbox .inp-cbx:checked + .cbx span:first-child svg {
+  stroke-dashoffset: 0;
 }
 </style>
